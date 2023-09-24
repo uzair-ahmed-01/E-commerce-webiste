ac2b04830550ce4e381bc1a1a92df4e65682f8d9
uzair-ahmed-01 <ansariuzair4151@gmail.com>
2023-09-25 01:18:16 +0530

    product section design

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
 
 menuItems.forEach((item, index) => {
-console.log(index)
     item.addEventListener("click", () => {
         wrapper.style.transform = `translateX(${-100 * index}vw)`;
     })
