9232fc8f540143e2e17fa0dbd334bd8f89c679d9
uzair-ahmed-01 <ansariuzair4151@gmail.com>
2023-09-26 00:13:47 +0530

    footer added now our responsive E-commerce site is complted its made just for brushup html,css,js

--- app.js
+++ app.js
@@ -135,9 +135,9 @@ const payment = document.querySelector(".payment");
 const close = document.querySelector(".close");
 
 productButton.addEventListener("click", () => {
-  payment.style.display = "flex";
+    payment.style.display = "flex";
 });
 
 close.addEventListener("click", () => {
-  payment.style.display = "none";
+    payment.style.display = "none";
 });
